38c76b3cd19ffd0c0336b6895ff99396ebf78a13
Vo Thanh Tung <thanhtungka91@gmail.com>
2016-09-17 10:04:11 +0900

    add static page

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'users#index'
+  root 'static_pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
