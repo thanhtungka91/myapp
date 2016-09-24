9a88d2a2e4e89200adbc5bf1531163116299a8fb
Vo Thanh Tung <thanhtungka91@gmail.com>
2016-09-25 02:04:15 +0900

    add remember me

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   #remember token -> here is function
-  attr_accessor :remember_tokenremember_token
+  attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
   has_many :microposts
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
+#encrypt passowrd
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
@@ -22,13 +22,20 @@ class User < ActiveRecord::Base
   def self.new_token
     SecureRandom.urlsafe_base64
   end
-
+  # get token-> encrypte token -> update to remember digest
+  # co ban la hieu -> truyen vao thang remeber token -> save vao thang remember digest
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
+  # pass rember_token
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
