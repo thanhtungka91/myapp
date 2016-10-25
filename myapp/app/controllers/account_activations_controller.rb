b0b387f0d40c030adcfa1bf646a985cb568b3dcd
Vo Thanh Tung <thanhtungka91@gmail.com>
2016-10-26 02:11:37 +0900

    fix htpps and time

--- myapp/app/controllers/account_activations_controller.rb
+++ myapp/app/controllers/account_activations_controller.rb
@@ -1,9 +1,10 @@
 class AccountActivationsController < ApplicationController
   def edit
+    # after verify-> user shoule be logged 
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.update_attribute(:activated_at, Time.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
